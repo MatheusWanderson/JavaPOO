0ea31155e2edf42461e8a675a3cb2988c6f4f2d6
Wanderson Matheus da Silva <wandersonmatheus@outlook.com.br>
2021-12-14 19:59:58 -0300

    Aula04

---
+++ Aula04/src/aula04/Caneta.java
@@ -0,0 +1,29 @@
+
+package aula04;
+
+public class Caneta {
+    
+    public String modelo;
+    private float ponta;
+    
+    public String modelo() {
+        return this.modelo;
+        
+    }
+    public void setModelo(String m){
+        this.modelo = m;
+    }
+    public float getPonta() {
+        return this.ponta;
+    }
+    public void setPonta(float p){
+        this.ponta = p;
+    }
+    public void status() {
+        System.out.println(" SOBRE A CANETA ");
+        System.out.println(" Modelo: "+ this.modelo);
+        System.out.println(" Ponta: "+ this.ponta);
+        
+    
+}
+}
