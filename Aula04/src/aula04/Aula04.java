b8c75085c8cacea8ebb4589f2824a227f5bbedd1
Wanderson Matheus da Silva <wandersonmatheus@outlook.com.br>
2021-12-14 21:00:33 -0300

    Aula04

--- Aula04/src/aula04/Aula04.java
+++ Aula04/src/aula04/Aula04.java
@@ -1,13 +1,19 @@
-
 package aula04;
 
 public class Aula04 {
 
     public static void main(String[] args) {
         Caneta c1 = new Caneta();
-        c1.setModelo("BIC");
-        c1.status();
-  
+
+        //c1.setModelo("BIC");
+
+       // c1.setPonta(0.5f);
+
+       // System.out.println(" tenho uma caneta " + c1.getModelo() + " Ponta "
+                //+ c1.getPonta());
+        
+       c1.status();
+
     }
-    
+
 }
--- Aula04/src/aula04/Caneta.java
+++ Aula04/src/aula04/Caneta.java
@@ -1,29 +1,51 @@
-
 package aula04;
 
 public class Caneta {
-    
-    public String modelo;
+
+    private String modelo;
     private float ponta;
+    private String cor;
+    private boolean tampada;
     
-    public String modelo() {
-        return this.modelo;
+    public Caneta() {  //  METODO CONSTRUTOR 
+        this.tampar();
+        this.cor = ("Azul");
+        
+        
         
     }
-    public void setModelo(String m){
+
+    public String getModelo() {
+        return this.modelo;
+
+    }
+
+    public void setModelo(String m) {
         this.modelo = m;
     }
+
     public float getPonta() {
         return this.ponta;
     }
-    public void setPonta(float p){
+
+    public void setPonta(float p) {
         this.ponta = p;
     }
+    
+    public void tampar(){
+        this.tampada = true;
+    }
+    
+    public void destampar() {
+        this.tampada = false;
+    }
+
     public void status() {
         System.out.println(" SOBRE A CANETA ");
-        System.out.println(" Modelo: "+ this.modelo);
-        System.out.println(" Ponta: "+ this.ponta);
-        
-    
-}
+        System.out.println(" Modelo: " + this.getModelo());
+        System.out.println(" Ponta: " + this.getPonta());
+        System.out.println(" Cor: " + this.cor);
+        System.out.println(" Tampada? " + this.tampada);
+
+    }
 }
